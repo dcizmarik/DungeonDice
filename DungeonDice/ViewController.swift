508e5b783a20a87172bc7c325760a996e308d5ee
Daniel Cizmarik <dcizmarik@daniels-macbook-pro.local>
2019-01-28 18:17:58 -0500

    Main app features completed, works on all screens.

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func rollRandomNumber (maximum: Int) -> Int {
+        return Int.random(in: 1...maximum)
+    }
+    
+    @IBAction func fourSided(_ sender: UIButton) {
+        messageLabel.text = "You rolled a 4-Sided Die and got a \(rollRandomNumber(maximum: 4))."
+    }
+    
+    @IBAction func sixSided(_ sender: UIButton) {
+        messageLabel.text = "You rolled a 6-Sided Die and got a \(rollRandomNumber(maximum: 6))."
+    }
+    
+    @IBAction func eightSided(_ sender: UIButton) {
+         messageLabel.text = "You rolled a 8-Sided Die and got a \(rollRandomNumber(maximum: 8))."
+    }
+    
+    @IBAction func tenSided(_ sender: UIButton) {
+         messageLabel.text = "You rolled a 10-Sided Die and got a \(rollRandomNumber(maximum: 10))."
+    }
+    
+    @IBAction func twelveSided(_ sender: UIButton) {
+         messageLabel.text = "You rolled a 12-Sided Die and got a \(rollRandomNumber(maximum: 12))."
+    }
+    
+    @IBAction func twentySided(_ sender: UIButton) {
+         messageLabel.text = "You rolled a 20-Sided Die and got a \(rollRandomNumber(maximum: 20))."
+    }
+    
+    @IBAction func oneHundredSided(_ sender: UIButton) {
+         messageLabel.text = "You rolled a 100-Sided Die and got a \(rollRandomNumber(maximum: 100))."
+    }
 
 }
 
